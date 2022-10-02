ca19e167b18071573e2f6f032fe9884b82475218
sungwoonpark0502 <park.3035@osu.edu>
2022-10-02 10:47:46 -0400

    front

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-const image_input = document.querySelector("#image-input");
-image_input.addEventListener("change", function() {
-  const reader = new FileReader();
-  reader.addEventListener("load", () => {
-    const uploaded_image = reader.result;
-    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
-  });
-  reader.readAsDataURL(this.files[0]);
-});
+var loadFile = function(event) {
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
+
+function postData(input) {
+    $.ajax({
+        type: "POST",
+        url: "test.py",
+        data: { param: input },
+        success: callbackFunc
+    });
+}
+
+function callbackFunc(response) {
+    // do something with the response
+    console.log(response);
+}
+
+postData('data to process');
+
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
+
+function myfunction_clickevent(){
+    $.ajax({
+        url: "test.py",
+        context: document.body
+    });
+}
+
+function myfunction_onload(){
+    $.ajax({
+        url: "test.py",
+         context: document.body
+        })
+    }
